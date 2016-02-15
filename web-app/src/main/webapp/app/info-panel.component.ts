256b4e4f00747aaf6ee8715395cb1c100e7eba91
Jacob Ferrero <jacobaferrero@gmail.com>
2016-02-15 12:21:24 -0800

    Ayinde Rules the World

---
+++ web-app/src/main/webapp/app/info-panel.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'info-panel',
+	templateUrl: 'app/info-panel.component.html',
+	styleUrls: ['app/info-panel.component.css']
+})
+export class InfoPanelComponent { }
--- web-app/src/main/webapp/app/main.component.ts
+++ web-app/src/main/webapp/app/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from 'angular2/core';
 import { OffersComponent } from './offers.component';
 import { BannerComponent } from './banner.component';
+import { InfoPanelComponent } from './info-panel.component';
 
 @Component({
 	templateUrl: 'app/main.component.html',
 	styleUrls: ['app/main.component.css'],
-	directives: [OffersComponent, BannerComponent]
+	directives: [OffersComponent, BannerComponent, InfoPanelComponent]
 })
 export class MainComponent { }
