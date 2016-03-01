ab0da494a9ffdd90fcec9bc12695810cc8083e71
edngroup <edngroup.vn@gmail.com>
2016-03-01 22:53:10 +0700

    EDN group update 20160301

--- web-app/src/main/webapp/app/offer-card.component.ts
+++ web-app/src/main/webapp/app/offer-card.component.ts
@@ -1,5 +1,17 @@
-import { Component, Input, AfterViewInit, ElementRef } from 'angular2/core';
+import { Component, View, Input, AfterViewInit, ElementRef } from 'angular2/core';
 import { ROUTER_DIRECTIVES } from 'angular2/router';
+import {Http, Headers, HTTP_PROVIDERS, BaseRequestOptions, RequestOptions} from 'angular2/http';
+//import { Observable } from 'rxjs/Observable';
+import {InforService} from './infor.service';
+
+var firstHeaders = new Headers();
+firstHeaders.append('Content-Type', 'application/json;charset=UTF-8');
+
+class MyOptions extends BaseRequestOptions {
+  headers: Headers = firstHeaders
+}
+
+var checkFirst = true;
 
 @Component({
 	selector: 'offer-card',
@@ -8,25 +20,37 @@ import { ROUTER_DIRECTIVES } from 'angular2/router';
 	directives: [ROUTER_DIRECTIVES]
 })
 export class OfferCardComponent implements AfterViewInit {
+  	@Input() public position;
     @Input() public offer;
     @Input() public resizeCallback;
-
-    ngAfterViewInit() {
-        Waves.displayEffect();
+    @Input() public deleteCallback;
+    @Input() public profile;
+    //private token = 'ya29.OwK_gZu6kwBy5Q_N5GkTZvVC1aNJinY4mNl9i3P2joKaXt5UqdFbXusCu0wW1CExbzlEX1U';
+ 	filesToUpload: Array<File>;	
+ 	
+	constructor(inforService: InforService, http: Http, element: ElementRef) {
+		this.inforService = inforService;
+        this.http = http;
+        this.element = element.nativeElement;   
+        this.filesToUpload = [];     
     }
-
+    ngAfterViewInit() {
+        Waves.displayEffect();        
+      if(this.isProfile() && this.position == 0 && checkFirst){
+      	checkFirst = false;
+		$('.modal-trigger').leanModal();
+      }
+    }
+    
     public getPrice() {
         var offer = this.offer;
         var str;
-
-        if (offer.price < 0) {
+        if(offer.price < 0) {
             str = 'Make $' + Math.abs(offer.price);
         } else if (offer.price === 0) {
             return 'Free';
-        } else if (offer.price > 0) {
-            str = '$' + offer.price
         } else {
-            return 'Ask';
+            str = '$' + offer.price
         }
 
         if (offer.unit) {
@@ -37,7 +61,7 @@ export class OfferCardComponent implements AfterViewInit {
     }
 
     public isRequest() {
-        return this.offer.price != null && this.offer.price < 0;
+        return this.offer.price < 0;
     }
 
     public getOfferTypeText() {
@@ -50,5 +74,73 @@ export class OfferCardComponent implements AfterViewInit {
 
     public loaded() {
         this.resizeCallback();
+    }
+
+    public getPosition() {
+    	return this.position;
+    }
+    
+    public isProfile() {
+    	if(this.profile == true)
+    		return true;
+    	else return false;    	
+    }
+    
+    public deleteOffer() {
+    	if(this.offer) {
+		    this.http.delete('http://queatz-snappy.appspot.com/api/me/offers/'+this.offer.id+'?auth='+this.inforService.getInforUser().auth, [])
+			    .map(res => {
+			    	if(res.status == 200){
+			    		Materialize.toast('Delete Offer success!', 4000);
+			    		this.deleteCallback(this.position,1);
+			    	}else{
+			    		Materialize.toast('Delete Offer fail!', 4000);
+			    	}
+			    	this.loaded();
+			    })
+			     .subscribe();
+		}
+    }
+    public uploadPhoto() {    	
+    	this.offer.hasPhoto = false;
+       	
+    	this.makeFileRequest("http://queatz-snappy.appspot.com/api/offer/"+ this.offer.id 
+    		+ "/photo?auth=" + this.inforService.getInforUser().auth, [], this.filesToUpload)
+    	.then(result => {
+    		 if(result){
+            	Materialize.toast('Upload Image success!', 4000);            	
+            	this.offer.hasPhoto = true;
+            }
+	    	else
+	    		Materialize.toast('Upload Image fail!', 4000);
+        }, (error) => {
+            console.error(error);
+        });
+    }
+    
+    makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+        return new Promise((resolve, reject) => {
+            var formData: any = new FormData();
+            var xhr = new XMLHttpRequest();
+            formData.append("photo", files[0], files[0].name);            
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.response));
+                    } else {
+                        reject(xhr.response);
+                    }
+                }
+            }              
+            xhr.open("PUT", url, true);
+            xhr.send(formData);
+        });
+    }
+    
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload = <Array<File>> fileInput.target.files;        
+    }
+    deleteImage(){
+    	Materialize.toast('Function empty!', 4000);
     }
 }
--- web-app/src/main/webapp/app/signin.component.ts
+++ web-app/src/main/webapp/app/signin.component.ts
@@ -1,17 +1,47 @@
-import {Component, AfterViewInit, ElementRef, Inject} from 'angular2/core';
+import {Component, AfterViewInit, ElementRef, Inject, provide} from 'angular2/core';
+import {ROUTER_DIRECTIVES, RouteParams, Router } from 'angular2/router';
+import { Http, Headers, HTTP_PROVIDERS, BaseRequestOptions, RequestOptions } from 'angular2/http';
+
+import {InforService} from './infor.service';
+
+var firstHeaders = new Headers();
+firstHeaders.append('Content-Type', 'application/x-www-form-urlencoded');	
+		
+
+class MyOptions extends BaseRequestOptions {
+  headers: Headers = firstHeaders
+}
 
 @Component({
     selector: 'signin',
-    template: '<div class="signin-wrapper"><div></div></div>',
-    styles: ['.signin-wrapper { display: inline-block; font-size: initial; text-shadow: none; text-align: center; padding-top: 4em; line-height: initial; }']
+    templateUrl: 'app/signin.component.html',
+    styleUrls: ['app/signin.component.css'],
+    viewProviders: [HTTP_PROVIDERS, provide(RequestOptions, {useClass: MyOptions})],
+    directives: [ROUTER_DIRECTIVES]
 })
 export class SigninComponent implements AfterViewInit {
-    constructor(elementRef: ElementRef) {
+	//private token = 'ya29.OwK_gZu6kwBy5Q_N5GkTZvVC1aNJinY4mNl9i3P2joKaXt5UqdFbXusCu0wW1CExbzlEX1U';
+    
+    constructor( 
+ 		inforService: InforService,
+        http: Http,
+        elementRef: ElementRef) {
+        this.inforService = inforService;
+        this.http = http;
         this.element = elementRef.nativeElement.children[0];
         this.signedIn = false; // todo -> auth factory
+        if(localStorage.getItem('myInfo')){
+	        this.localData = JSON.parse(localStorage.getItem('myInfo'));
+	        if(this.localData){
+	        	this.setglink(this.localData.googleUrl);
+	           	this.setgimage(this.localData.imageUrl);   
+	        }
+        }
     }
 
     ngAfterViewInit() {
+    	$('.modal-trigger').leanModal();
+    	        
         gapi.signin2.render(this.element, {
             width: 240,
             longtitle: true,
@@ -25,9 +55,31 @@ export class SigninComponent implements AfterViewInit {
             scope: 'profile email',
             redirect_uri: 'http://localhost:3000' //todo
         });
+                      
     }
-
+    getSignIn(){
+    	return this.signedIn;
+    }
+    getglink(){
+    	return this.glink;
+    }
+	setglink(link){
+		this.glink = link;
+	}
+	setgimage(image){
+		this.gimage = image;
+	}
     public onSuccess(googleUser) {
-        console.log(googleUser);
-    }
+       	var gemail = googleUser.getBasicProfile().getEmail();
+        var gtoken = googleUser.getAuthResponse().access_token;
+        //this.gimage = googleUser.getBasicProfile().getImageUrl();
+        
+        this.http.get('http://queatz-snappy.appspot.com/api/me' + '?email='+ gemail+ '&auth=' + gtoken)
+        	.map((res: Response) => res.json())
+        	.subscribe(dataInput => {
+        		this.setglink(dataInput.googleUrl);
+           		this.setgimage(dataInput.imageUrl);           		
+           		this.inforService.setInforUser(dataInput);           		
+        	});  
+    }    
 }
