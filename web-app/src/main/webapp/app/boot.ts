2a1b5ad1205ff6ed4d6636340829d5c3e03b1c6b
edngroup <edngroup.vn@gmail.com>
2016-03-11 00:33:27 +0700

    Update boot.ts
    //bootstrap

--- web-app/src/main/webapp/app/boot.ts
+++ web-app/src/main/webapp/app/boot.ts
@@ -1,5 +1,6 @@
-import { bootstrap }    from 'angular2/platform/browser'
+import { bootstrap }    from 'angular2/platform/browser';
 import { ROUTER_PROVIDERS } from 'angular2/router';
 import { AppComponent } from './app.component';
+import {InforService} from './infor.service';
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [ROUTER_PROVIDERS, InforService]);
