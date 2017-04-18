4952ad2c85c5be5306bb0e555f0679b031bcc2fc
Jacob Ferrero <jacobaferrero@gmail.com>
2017-04-17 23:11:34 -0500

    Add from profile

--- web-app/src/main/webapp/app/add-member.component.ts
+++ web-app/src/main/webapp/app/add-member.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, AfterViewInit } from '@angular/core';
 
 @Component({
     selector: 'add-member',
     templateUrl: 'app/add-member.component.html',
     styleUrls: ['app/add-member.component.css'],
 })
-export class AddMemberComponent {
+export class AddMemberComponent implements AfterViewInit {
     @Input() thing;
+
+    constructor(private elementRef: ElementRef) {}
+
+    ngAfterViewInit() {
+        $(this.elementRef.nativeElement).find('.tooltipped').tooltip({delay: 50});
+        $(this.elementRef.nativeElement).find('.modal').modal();
+    }
 }
